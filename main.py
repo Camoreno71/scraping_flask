d8103015cd97bb1865d69b6f28bf7c354c76081e
Carlos Moreno <camoreno71@ucatolica.edu.co>
2024-11-07 23:16:28 -0500

    add idea to review input

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def scrape_page():
     with sync_playwright() as p:
         # Iniciar el navegador y abrir una nueva página
-        browser = p.chromium.launch(headless=True)  # Puedes poner headless=True para que no se vea el navegador
+        browser = p.chromium.launch(headless=False)  # Puedes poner headless=True para que no se vea el navegador
         page = browser.new_page()
 
         # Navegar a la URL deseada
@@ -15,6 +15,18 @@ def scrape_page():
 
         p_element = page.query_selector('p.elementor-heading-title.elementor-size-default')
         text = p_element.inner_text()
+        input_selector = 'input.hfe-search-form__input[type="search"]'
+        print(input_selector)
+
+        page.fill(input_selector, 'texto de búsqueda')
+
+        button = page.query_selector('a.elementor-button.elementor-button-link.elementor-size-lg')
+        if button:
+            button.click() 
+            page.wait_for_load_state('networkidle')
+            h2_element = page.query_selector('h2.widget-title')
+            h2_text = h2_element.text_content()
+            print(h2_text)
 
     return f"<p>hola{text}</p>"
 
